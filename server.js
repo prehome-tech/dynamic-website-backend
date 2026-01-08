56bc8232df9ec5f471a31396cf6e44534f7dc2f5
prehomestorage <storage.prehome@gmail.com>
2026-01-08 18:49:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // --- CORS CONFIGURATION ---
 const allowedOrigins = [
   "https://demo.prehome.in",
-  "https://dynamic-website-react.onrender.com",
+  "https://dynamic-website-frontend.onrender.com",
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:4173",
